40a39de4bc84e8dc1c4869828dd3210941c02c14
shafikul <shaifk108@gmail.com>
2023-09-09 16:35:36 +0600

    Governing page modify

--- src/views/About/Governing.vue
+++ src/views/About/Governing.vue
@@ -4,6 +4,119 @@ import Navbar from '../../components/Navbar.vue';
 import LeftSideView from '../Home/LeftSideView.vue';
 import RightSideView from '../Home/RightSideView.vue';
 import Footer from '../../components/Footer.vue';
+
+const govorningDetalis = [
+  {
+    image: 'https://i.imgur.com/ZTkt4I5.jpg',
+    title: 'Card title',
+    subtitle: 'Subtitle',
+    detalis: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    linkAll: [
+      {
+        linkText: '+8801756867220',
+        icon: 'fa fa-mobile',
+      },
+      {
+        linkText: 'shafikul@gamil.com',
+        icon: 'fa fa-envelope-o'
+      },
+
+    ]
+
+  },
+  {
+    image: 'https://i.imgur.com/ZTkt4I5.jpg',
+    title: 'Card title',
+    subtitle: 'Sub title',
+    detalis: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    linkAll: [
+      {
+        linkText: '+8801756867220',
+        icon: 'fa fa-mobile',
+      },
+      {
+        linkText: 'shafikul@gamil.com',
+        icon: 'fa fa-envelope-o'
+      },
+
+    ]
+
+  },
+  {
+    image: 'https://i.imgur.com/ZTkt4I5.jpg',
+    title: 'Card title',
+    subtitle: '',
+    detalis: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    linkAll: [
+      {
+        linkText: '+8801756867220',
+        icon: 'fa fa-mobile',
+      },
+      {
+        linkText: 'shafikul@gamil.com',
+        icon: 'fa fa-envelope-o'
+      },
+
+    ]
+
+  },
+  {
+    image: 'https://i.imgur.com/ZTkt4I5.jpg',
+    title: 'Card title',
+    subtitle: '',
+    detalis: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    linkAll: [
+      {
+        linkText: '+8801756867220',
+        icon: 'fa fa-mobile',
+      },
+      {
+        linkText: 'shafikul@gamil.com',
+        icon: 'fa fa-envelope-o'
+      },
+
+    ]
+
+  },
+  {
+    image: 'https://i.imgur.com/ZTkt4I5.jpg',
+    title: 'Card title',
+    subtitle: '',
+    detalis: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    linkAll: [
+      {
+        linkText: '+8801756867220',
+        icon: 'fa fa-mobile',
+      },
+      {
+        linkText: 'shafikul@gamil.com',
+        icon: 'fa fa-envelope-o'
+      },
+
+    ]
+
+  },
+  {
+    image: 'https://i.imgur.com/ZTkt4I5.jpg',
+    title: 'Card title',
+    subtitle: '',
+    detalis: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    linkAll: [
+      {
+        linkText: '+8801756867220',
+        icon: 'fa fa-mobile',
+      },
+      {
+        linkText: 'shafikul@gamil.com',
+        icon: 'fa fa-envelope-o'
+      },
+
+    ]
+
+  },
+  
+]
+
 </script>
 
 <template>
@@ -35,7 +148,26 @@ import Footer from '../../components/Footer.vue';
 <!-- All Work Start -->
         <div class="container-fluid">
           <div class="row">
-            <div class="col-md-12"></div>
+            <div class="col-md-12">
+                <div class="container mx-auto mt-4">
+                  <div class="row">
+                    <div class="col-md-6" v-for="(allDetalis, index) in govorningDetalis" :key="index">
+                      <div class="card">
+                        <img :src="allDetalis.image" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">{{ allDetalis.title }}</h5>
+                          <h6 class="card-subtitle mb-2">{{ allDetalis.subtitle }}</h6>
+                          <p class="card-text">{{ allDetalis.detalis.split(' ').slice(0, 10).join(' ') }} ....</p>
+                          <hr>
+                          <div class="" v-for="(detalisLink, index) in allDetalis.linkAll" :key="index">
+                            <RouterLink to="/" class="btn mr-2"><i :class="detalisLink.icon"></i> {{ detalisLink.linkText }}</RouterLink> 
+                          </div>
+                        </div>
+                      </div>
+                    </div>    
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -53,5 +185,39 @@ import Footer from '../../components/Footer.vue';
 <style scoped>
 .designLine{
   font-size: 3rem;
+}
+:root {
+  --gradient: linear-gradient(to left top, #DD2476 10%, #FF512F 90%) !important;
+}
+
+body {
+  background: #111 !important;
+}
+
+.card {
+  background: #222;
+  border: 1px solid #dd2476;
+  color: rgba(250, 250, 250, 0.8);
+  margin-bottom: 2rem;
+}
+
+.btn {
+  border: 5px solid;
+  border-image-slice: 1;
+  background: var(--gradient) !important;
+  -webkit-background-clip: text !important;
+  -webkit-text-fill-color: #71f086 !important;
+  border-image-source:  var(--gradient) !important; 
+  text-decoration: none;
+  transition: all .4s ease;
+}
+
+.btn:hover, .btn:focus {
+      background: var(--gradient) !important;
+  -webkit-background-clip: none !important;
+  -webkit-text-fill-color: #fff !important;
+  border: 5px solid #fff !important; 
+  box-shadow: #222 1px 0 10px;
+  text-decoration: underline;
 }
 </style>
--- src/views/ExamView.vue
+++ src/views/ExamView.vue
@@ -4,4 +4,87 @@ const ExamPage = 'This is a exam page'
 
 <template>
     <h1 class="text-4xl text-center font-bold bg-indigo-800">{{ ExamPage }}</h1>
+
+
+    <div class="counter">
+        <div class="counter-icon">
+            <i class="fa-solid fa-user-graduate"></i>
+        </div>
+        <span class="counter-value">17</span>
+        <h3>শিক্ষার্থী </h3>
+    </div>
 </template>
+<style scoped>
+.counter{
+    text-align: center;
+    height: 210px;
+    width: 210px;
+    padding: 7px 4px 0;
+    margin: 0 auto;
+    position: relative;
+    z-index: 1;
+}
+.counter:before{
+    content: '';
+    background-color: #fff;
+    height: 157px;
+    width: 157px;
+    border-radius: 15px;
+    border: 5px solid #202c45;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2) inset;
+    transform: translateX(-50%) translateY(-50%) rotate(45deg);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    z-index: -1;
+}
+.counter .counter-icon{
+    color: #fff;
+    background: linear-gradient(#f2184f,#d50d3f);
+    line-height: 70px;
+    font-size: 30px;
+    height: 70px;
+    width: 70px;
+    margin: 0 auto 8px;
+    border-radius: 19px 0 50px;
+    transform: rotate(45deg);
+}
+.counter .counter-icon i{
+    transform: rotate(-45deg);
+}
+.counter .counter-value{
+    font-size: 30px;
+    font-weight: 700;
+    letter-spacing: 1px;
+    margin: 0 0 5px 0;
+    display: block;
+    font-family: 'Poppins';
+    color: #202c45;
+}
+.counter h3{
+    color: #fff;
+    background: linear-gradient(#22253a,#424971);
+    font-size: 18px;
+    letter-spacing: 1px;
+    text-transform: capitalize;
+    padding: 10px 5px;
+    margin: 0;
+    border-radius: 0 0 20px 20px;
+    position: relative;
+}
+.counter h3:before, .counter h3:after{
+    content: "";
+    background: linear-gradient(to right bottom, transparent 49%,#424971 50%);
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    top: -20px;
+    left: 0;
+    z-index: -2;
+}
+.counter h3:after{
+    transform: rotateY(180deg);
+    left: auto;
+    right: 0;
+} 
+</style>
